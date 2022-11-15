0bac8524a51069738bcab48699c6fc5c26df9338
Billy Cheng <chengpitchi@yahoo.com>
2022-11-16 01:57:38 +1030

    gen html and validations

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern'); 
+const Intern = require('./lib/intern');
+const generateHTML = require('./lib/generateHTML');  
 
 const fs = require('fs'); 
 const inquirer = require('inquirer'); 
+const { Console } = require('console');
 
 const engineers = new Array(); 
 const interns = new Array(); 
+let objManager; 
 
 const managerInfo = [
     {
       type: 'input',
       message: 'What is the Manager\'s name?',
       name: 'name',
+      validate(input) {
+        if (input !== '') return true;  
+        throw Error('The field cannot be empty.');
+        },
     },
     {
       type: 'input',
       message: 'What is the Manager\'s ID?',
       name: 'id',
+      validate(input) {
+        if (input !== '') return true;  
+        throw Error('The field cannot be empty.');
+        },
     },
     {
       type: 'input',
       message: 'What is the Manager\'s email address?',
       name: 'email',
+      validate(input) {
+            if (input === '') {
+                throw Error('The field cannot be empty.');
+                return false; 
+            }
+
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+                return true; 
+            }
+
+            throw Error('Please input correct email format.');
+        },
     },
     {
       type: 'input',
       message: 'What is the Manager\'s office number',
       name: 'officeNumber',
-    },
-    {
-      type: 'list',
-      message: 'What kihd of license should your project have?',
-      name: 'license',
-      choices: ['None', 'MIT', 'JavaScript', 'Apache 2.0', 'Boost Software 1.0', 'Perl', 'Open Database'],
-    },
+      validate(input) {
+        if (input !== '') return true;  
+        throw Error('The field cannot be empty.');
+        },
+    }
     ];
 
 const typeQuestion = [    
@@ -50,21 +71,45 @@ const engineerInfo = [
         type: 'input',
         message: 'What is the Engineer\'s name?',
         name: 'name',
+        validate(input) {
+            if (input !== '') return true;  
+            throw Error('The field cannot be empty.');
+            },
     },
     {
         type: 'input',
         message: 'What is the Engineer\'s ID?',
         name: 'id',
+        validate(input) {
+            if (input !== '') return true;  
+            throw Error('The field cannot be empty.');
+            },
     },
     {
         type: 'input',
         message: 'What is the Engineer\'s email address?',
         name: 'email',
+        validate(input) {
+            if (input === '') {
+                throw Error('The field cannot be empty.');
+                return false; 
+            }
+
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+                return true; 
+            }
+
+            throw Error('Please input correct email format.');
+        },
     },
     {
         type: 'input',
         message: 'What is the Engineer\'s Github username',
         name: 'github',
+        validate(input) {
+            if (input !== '') return true;  
+            throw Error('The field cannot be empty.');
+            },
     },
     ];
 
@@ -73,44 +118,97 @@ const InternInfo = [
         type: 'input',
         message: 'What is the Intern\'s name?',
         name: 'name',
+        validate(input) {
+            if (input !== '') return true;  
+            throw Error('The field cannot be empty.');
+            },
     },
     {
         type: 'input',
         message: 'What is the Intern\'s ID?',
         name: 'id',
+        validate(input) {
+            if (input !== '') return true;  
+            throw Error('The field cannot be empty.');
+            },
     },
     {
         type: 'input',
         message: 'What is the Intern\'s email address?',
         name: 'email',
+        validate(input) {
+            if (input === '') {
+                throw Error('The field cannot be empty.');
+                return false; 
+            }
+
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+                return true; 
+            }
+
+            throw Error('Please input correct email format.');
+        },
     },
     {
         type: 'input',
         message: 'What is the Intern\'s School Name',
         name: 'school',
+        validate(input) {
+            if (input !== '') return true;  
+            throw Error('The field cannot be empty.');
+            },
     },
     ];    
-  
-  inquirer
+
+function addTeamMember() {
+    inquirer.prompt(typeQuestion)
+        .then((response) => {
+                switch (response.type) {
+                    case 'Engineer':
+                        inquirer.prompt(engineerInfo)
+                            .then((response) => {
+                                console.log('add engineer'); 
+                                engineers.push(new Engineer(response.name, response.id, response.email, response.github))
+                                addTeamMember(); 
+                            }); 
+                        break; 
+                    case 'Intern':
+                        inquirer.prompt(InternInfo)
+                            .then((response) => {
+                                console.log('add intern');
+                                interns.push(new Intern(response.name, response.id, response.email, response.school))
+                                addTeamMember(); 
+                            }); 
+                        break; 
+                    default: 
+                        writeToFile(objManager, engineers, interns); 
+                }
+            }
+        )
+}
+
+function writeToFile(objManager, engineers, interns) {
+    const htmlContent = generateHTML(objManager, engineers, interns); 
+
+    fs.writeFile('./dist/index.html', htmlContent, (err) =>
+    err ? console.error(err) : console.log('Congratulations! Your team profile page is created.'))    
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    console.log('Welcome to Team Profile Generator!'); 
+    console.log('Please answer the following questions to generate your team profile:'); 
+
+    inquirer
     .prompt(managerInfo)
     .then((response) => {
-        managerObj = new Manager(response.name, response.id, response.email, response.officeNumber)
+        objManager = new Manager(response.name, response.id, response.email, response.officeNumber)
     })
     .then(() => {
         addTeamMember(); 
     });
-/*  
-  // TODO: Create a function to write README file
-  function writeToFile(fileName, data) {
-    const readmeContent = generateMarkdown(data); 
-  
-    fs.writeFile(fileName, readmeContent, (err) =>
-    err ? console.error(err) : console.log('Congratulations! Your professional README file is generated.'))    
-  }
-*/
-  // TODO: Create a function to initialize app
-  function init() {}
-  
-  // Function call to initialize app
-  init();
+}
+
+// Function call to initialize app
+init();
   
